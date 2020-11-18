d74a7095cae97249f16ef80df9bcf656e302947a
Renato Mazzilli <39441015+Rokziell@users.noreply.github.com>
2020-11-17 22:50:39 -0500

    Portfolio
    V1.0

---
+++ precache-manifest.2d424a767346c9e1f158329198da7b38.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c67450b41e12befd9a0a00e3b41d850",
+    "url": "/index.html"
+  },
+  {
+    "revision": "198493a44311aecb121a",
+    "url": "/static/css/main.8ab4de4a.chunk.css"
+  },
+  {
+    "revision": "f36584d43dfa4a2263ae",
+    "url": "/static/js/2.7f4a27df.chunk.js"
+  },
+  {
+    "revision": "0cf5ba2c00bbc0b01089c885c528fd0c",
+    "url": "/static/js/2.7f4a27df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "198493a44311aecb121a",
+    "url": "/static/js/main.19099317.chunk.js"
+  },
+  {
+    "revision": "e2c6e531b8324ac17d6c",
+    "url": "/static/js/runtime-main.206ac5b9.js"
+  },
+  {
+    "revision": "0da459c4fb1efbe27dd35e7e5e51760f",
+    "url": "/static/media/foto_01.0da459c4.png"
+  },
+  {
+    "revision": "2e6fb22aa870ee195f5d651fc07cb4d5",
+    "url": "/static/media/foto_01.2e6fb22a.png"
+  },
+  {
+    "revision": "7a2b429c6fce39d8f71e34fe718b0f3a",
+    "url": "/static/media/foto_01.7a2b429c.png"
+  },
+  {
+    "revision": "a9df2b16629e1a1fe5ccd5694b6fa5d9",
+    "url": "/static/media/foto_01.a9df2b16.png"
+  },
+  {
+    "revision": "13c97fe7d142af553d8456a6f907b62a",
+    "url": "/static/media/foto_02.13c97fe7.png"
+  },
+  {
+    "revision": "3e0fc568abded501b9c1aba02ceadb46",
+    "url": "/static/media/foto_02.3e0fc568.png"
+  },
+  {
+    "revision": "7f2465343f7612b234777e902b335fa3",
+    "url": "/static/media/foto_02.7f246534.png"
+  },
+  {
+    "revision": "a208905783ce2ccc90773a5c6eb47adb",
+    "url": "/static/media/foto_02.a2089057.png"
+  },
+  {
+    "revision": "5e4a154d84329d32d52bd798432db983",
+    "url": "/static/media/foto_03.5e4a154d.png"
+  },
+  {
+    "revision": "7a6ab548a20e92b6bde9c1994e23c7b1",
+    "url": "/static/media/foto_03.7a6ab548.png"
+  },
+  {
+    "revision": "d2772fc5af38ef40aaa618030d9af1b8",
+    "url": "/static/media/foto_03.d2772fc5.png"
+  },
+  {
+    "revision": "e64043de6d10e025c7105cb0c559505f",
+    "url": "/static/media/foto_03.e64043de.png"
+  },
+  {
+    "revision": "3b84c5799398459cd8a3c17cbbf6c292",
+    "url": "/static/media/foto_04.3b84c579.png"
+  },
+  {
+    "revision": "905b41ff59bc18a69a112bc31e5c1ea5",
+    "url": "/static/media/foto_04.905b41ff.png"
+  },
+  {
+    "revision": "92a5c1c2caee033fcc65dad1793c5361",
+    "url": "/static/media/foto_04.92a5c1c2.png"
+  },
+  {
+    "revision": "d743c209222d9f12e3e2e634d7e77fa4",
+    "url": "/static/media/foto_04.d743c209.png"
+  },
+  {
+    "revision": "0078836edb0c33c567da948905a6a828",
+    "url": "/static/media/foto_05.0078836e.png"
+  },
+  {
+    "revision": "85c1714e6cf39f2efff79ac194987348",
+    "url": "/static/media/foto_05.85c1714e.png"
+  },
+  {
+    "revision": "d1564a84a16d15a92c678bd14b394c4d",
+    "url": "/static/media/foto_05.d1564a84.png"
+  },
+  {
+    "revision": "54afc55577c0c78851082f457b92a738",
+    "url": "/static/media/foto_06.54afc555.png"
+  },
+  {
+    "revision": "5ab70d7a4aa47671120124553c7943e3",
+    "url": "/static/media/foto_06.5ab70d7a.png"
+  },
+  {
+    "revision": "cea718d53e61298d74574f04d67ad122",
+    "url": "/static/media/foto_06.cea718d5.png"
+  },
+  {
+    "revision": "a6f3eb7a0eec4ed563215d107421a780",
+    "url": "/static/media/foto_07.a6f3eb7a.png"
+  },
+  {
+    "revision": "cc7e8123843bdb91b4746ce0f240e996",
+    "url": "/static/media/foto_07.cc7e8123.png"
+  },
+  {
+    "revision": "3099f2da965ba76e684848a05abf573b",
+    "url": "/static/media/foto_08.3099f2da.png"
+  },
+  {
+    "revision": "3b1d031e9f192cec5256f3cbebb62b48",
+    "url": "/static/media/foto_08.3b1d031e.png"
+  },
+  {
+    "revision": "85faece528ab02f6a21e75b2cb917350",
+    "url": "/static/media/foto_09.85faece5.png"
+  },
+  {
+    "revision": "970b1d38e1d23432d4df1502f8506783",
+    "url": "/static/media/foto_09.970b1d38.png"
+  },
+  {
+    "revision": "8f64295f46db421c3ec6fc04631855d0",
+    "url": "/static/media/foto_10.8f64295f.png"
+  },
+  {
+    "revision": "ac31830eea8bcc16001bfb2450b1bdc6",
+    "url": "/static/media/foto_10.ac31830e.png"
+  },
+  {
+    "revision": "28ece6042ee014723a7c5ffe18580892",
+    "url": "/static/media/foto_11.28ece604.png"
+  },
+  {
+    "revision": "c67c6390ecb71ec43cac2cc9fd632bf6",
+    "url": "/static/media/foto_12.c67c6390.png"
+  },
+  {
+    "revision": "ea2aa9c55b1a8944db1a54837e14023e",
+    "url": "/static/media/foto_13.ea2aa9c5.png"
+  },
+  {
+    "revision": "ea2aa9c55b1a8944db1a54837e14023e",
+    "url": "/static/media/foto_14.ea2aa9c5.png"
+  },
+  {
+    "revision": "ab15fa36092578a76d7f8b330cd07035",
+    "url": "/static/media/foto_15.ab15fa36.png"
+  },
+  {
+    "revision": "ea20a8fbd3499b10e034e76b3e10f236",
+    "url": "/static/media/logo_renato.ea20a8fb.svg"
+  },
+  {
+    "revision": "9b9dd222e30ac132641698fd04238cd5",
+    "url": "/static/media/renativiris.9b9dd222.jpg"
+  }
+]);
